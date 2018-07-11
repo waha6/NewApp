0d7ce6cc7d1f1911018bd95d2d769704675dac86
Abdul Wahab <a6dulwahab@gmail.com>
2018-07-11 05:14:31 +0500

    finish some improvement

--- public/scripts/signin-up.js
+++
@@ -1,1 +0,0 @@
-function togglee(e){1==e?(document.querySelector(".register-form").style.display="none",document.querySelector(".login-form").style.display="block"):(document.querySelector(".register-form").style.display="block",document.querySelector(".login-form").style.display="none")}
